b296e4456598e79faa25ac3982c753c7dee6b895
WillDevE <91845310+WillDevE@users.noreply.github.com>
2023-06-09 18:21:13 +0800

    AAAAAAAAAAAAAAAAAAAAAAAAAAA

--- scripts.js
+++ scripts.js
@@ -1,20 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Dark mode toggle functionality
     const darkModeToggle = document.querySelector('.dark-mode-toggle');
     const body = document.querySelector('body');
-
+  
+    // Check if user preference is stored in localStorage
+    const userPref = localStorage.getItem('colorMode');
+    if (userPref === 'dark') {
+      enableDarkMode();
+    } else if (userPref === 'light') {
+      enableLightMode();
+    }
+  
     darkModeToggle.addEventListener('click', function() {
-        body.classList.toggle('dark-mode');
-
-        const moonIcon = darkModeToggle.querySelector('.fa-moon');
-        const sunIcon = darkModeToggle.querySelector('.fa-sun');
-
-        if (body.classList.contains('dark-mode')) {
-            moonIcon.style.animation = 'hideIcon 0.3s forwards';
-            sunIcon.style.animation = 'showIcon 0.3s forwards';
-        } else {
-            moonIcon.style.animation = 'showIcon 0.3s forwards';
-            sunIcon.style.animation = 'hideIcon 0.3s forwards';
-        }
+      if (body.classList.contains('dark-mode')) {
+        enableLightMode();
+      } else {
+        enableDarkMode();
+      }
     });
-});
+  
+    function enableDarkMode() {
+      body.classList.add('dark-mode');
+      localStorage.setItem('colorMode', 'dark');
+  
+      const moonIcon = darkModeToggle.querySelector('.fa-moon');
+      const sunIcon = darkModeToggle.querySelector('.fa-sun');
+      moonIcon.style.animation = 'hideIcon 0.3s forwards';
+      sunIcon.style.animation = 'showIcon 0.3s forwards';
+    }
+  
+    function enableLightMode() {
+      body.classList.remove('dark-mode');
+      localStorage.setItem('colorMode', 'light');
+  
+      const moonIcon = darkModeToggle.querySelector('.fa-moon');
+      const sunIcon = darkModeToggle.querySelector('.fa-sun');
+      moonIcon.style.animation = 'showIcon 0.3s forwards';
+      sunIcon.style.animation = 'hideIcon 0.3s forwards';
+    }
+  });
+  
