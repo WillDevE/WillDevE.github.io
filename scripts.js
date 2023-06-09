2bac1cd5c246a9ea7994da73e448919454079e36
WillDevE <91845310+WillDevE@users.noreply.github.com>
2023-06-09 16:06:18 +0800

    still fixing the light mode dark mode toggle

--- scripts.js
+++ scripts.js
@@ -1,23 +1,20 @@
-// Dark mode toggle functionality
 document.addEventListener('DOMContentLoaded', function() {
     // Dark mode toggle functionality
     const darkModeToggle = document.querySelector('.dark-mode-toggle');
     const body = document.querySelector('body');
-  
-    darkModeToggle.addEventListener('click', function() {
-      body.classList.toggle('dark-mode');
-  
-      const moonIcon = darkModeToggle.querySelector('.fa-moon');
-      const sunIcon = darkModeToggle.querySelector('.fa-sun');
-  
-      if (body.classList.contains('dark-mode')) {
-        moonIcon.style.display = 'none';
-        sunIcon.style.display = 'inline';
-      } else {
-        moonIcon.style.display = 'inline';
-        sunIcon.style.display = 'none';
-      }
-    });
-  });
 
-  
+    darkModeToggle.addEventListener('click', function() {
+        body.classList.toggle('dark-mode');
+
+        const moonIcon = darkModeToggle.querySelector('.fa-moon');
+        const sunIcon = darkModeToggle.querySelector('.fa-sun');
+
+        if (body.classList.contains('dark-mode')) {
+            moonIcon.classList.remove('animate');
+            sunIcon.classList.add('animate');
+        } else {
+            sunIcon.classList.remove('animate');
+            moonIcon.classList.add('animate');
+        }
+    });
+});
